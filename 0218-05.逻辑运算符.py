80780db61fb7fffa7afa0c9f6c7208a167af497c
blackplusy <happyming07@sina.com>
2019-02-18 15:21:03 +0800

    Add files via upload

---
+++ 0218-05.逻辑运算符.py
@@ -0,0 +1,10 @@
+#coding=utf-8
+print(1>=1 and 1==2)
+# 真 and 真 = 真
+# 真 and 假 = 假
+
+print(2>1 or 0>1)
+# 真 or 假 = 真
+
+print(not(3==2))
+# 非假 = 真
---
+++ 0218-06.成员运算符.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+a='simida'
+
+for i  in  a:
+	print(i)
+
+if 'd' in a:
+	print('d is here!')
+
+if 'c' not in a:
+	print('c is not here!')
