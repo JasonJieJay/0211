a170310dda9492eae28c5296ff678dced5668025
blackplusy <happyming07@sina.com>
2019-02-18 16:01:18 +0800

    Add files via upload

---
+++ 0218-07.条件分支.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+'''
+name=input('请输入wuli的名字：')
+if  name=='baga':
+	print('wuli的名字是baga')
+'''
+
+name=input('请输入您喜欢的主播：')
+'''
+if name=='chui':
+	print('为什么爱上了他')
+else:
+	print('还不是因为眼瞎')
+'''
+
+if name=='song':
+	print('不服来来杠啊！')
+elif name=='锤':
+	print('来啦老妹儿！')
+else:
+	print('赛郎黑又')
