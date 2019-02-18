909e0bec35f393918e6607fbde2e5d9a2b614314
blackplusy <happyming07@sina.com>
2019-02-18 17:43:33 +0800

    Add files via upload

---
+++ 0218-08.for循环.py
@@ -0,0 +1,20 @@
+#coding=utf-8
+
+for i  in  'aisb':
+	print(i)
+
+l=[1,2,'simida','gaga']
+
+for i in l:
+	print(i)
+
+#range函数
+#range(10)   0-9
+#range(1,10) 1-9
+
+print('*****')
+for i in range(10):
+	print(i)
+print('*****')
+for i in range(1,10):
+	print(i)
