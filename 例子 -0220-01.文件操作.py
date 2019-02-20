33f8fc406c7d9f555855a9046eb943c74fc113dd
blackplusy <happyming07@sina.com>
2019-02-20 09:46:37 +0800

    Create 例子 -0220-01.文件操作.py

---
+++ 例子 -0220-01.文件操作.py
@@ -0,0 +1,29 @@
+#coding=utf-8
+'''
+file=open('/home/heygor/0220/example','r')
+for i in file:
+	print(i)
+file.close()
+
+'''
+
+'''
+str1='my dear chui!'
+file=open('/home/heygor/0220/test','w')
+file.write(str1)
+file.close()
+print('执行完毕')
+'''
+file=open('/home/heygor/0220/test','a')
+file.write('come on baby!\n')
+file.write('come on baby!\n')
+file.write('come on baby!\n')
+file.write('come on baby!\n')
+file.close()
+
+
+
+
+
+
+
