003d1046064e8fc67572dc4414288f31b5e36c9c
blackplusy <happyming07@sina.com>
2019-02-22 10:03:18 +0800

    Add files via upload

---
+++ 例子-0222-02.模块操作.py
@@ -0,0 +1,11 @@
+#coding=utf-8
+import os
+import sys
+print(os.listdir())
+print(os.path.split('/var/log/syslog'))
+print(os.path.isfile('/var/log/syslog'))
+
+#sys模块
+print(sys.version)
+print(sys.path)
+
