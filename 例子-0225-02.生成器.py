9cbafc368c705db4a9e2ee7dd5c571fc64736d5a
blackplusy <happyming07@sina.com>
2019-02-25 10:39:34 +0800

    Add files via upload

---
+++ 例子-0225-02.生成器.py
@@ -0,0 +1,31 @@
+#coding=utf-8
+'''
+arr=(x for x in range(1,5))
+#print(arr)
+#for i in arr:
+#	print(i)
+print('next1 is')
+print(next(arr))
+print('next1 is')
+print(next(arr))
+print('next1 is')
+print(next(arr))
+'''
+def test():
+        n=1
+        print('first!')
+        yield n
+        n+=1
+        print('second！')
+        yield n
+        n+=1
+        print('third')
+        yield n
+
+a=test()
+print('next one is')
+print(next(a))
+print(next(a))
+
+print(next(a))
+print(next(a))
